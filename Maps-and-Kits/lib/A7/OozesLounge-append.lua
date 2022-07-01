e2cf8848d4469ceaa945e018d7d5e0633f43968f
11jo <79876551+11jo@users.noreply.github.com>
2022-07-01 03:14:07 +0200

    Up install code and first BG Areas

--- Maps-and-Kits/lib/A7/OozesLounge-append.lua
+++ Maps-and-Kits/lib/A7/OozesLounge-append.lua
@@ -1,3 +1,3 @@
-{"L#NS01", "@10000"}, //{"L#NS01", "OOZES - Prêtre de Juiblex fou"},
-{"L#NS02", "@10001"}, //{"L#NS02", "OOZES - loge"},
-{"L#NS03", "@10002"}, //{"L#NS03", "OOZES - Souterrain"},
+{"L#NS01", "OOZES - Prêtre de Juiblex fou"},
+{"L#NS02", "OOZES - loge"},
+{"L#NS03", "OOZES - Souterrain"},
